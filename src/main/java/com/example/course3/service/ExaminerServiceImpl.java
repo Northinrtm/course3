2a5767b42b247a0f0cb1d11c6bc1790d22433603
northinrtm <artsdurden@gmail.com>
2022-12-11 17:15:46 +0300

    без тестов

---
+++ src/main/java/com/example/course3/service/ExaminerServiceImpl.java
@@ -0,0 +1,28 @@
+package com.example.course3.service;
+
+import com.example.course3.domain.Question;
+import org.springframework.stereotype.Service;
+
+import java.util.*;
+
+@Service
+public class ExaminerServiceImpl implements ExaminerService {
+    private Random random = new Random();
+    private final QuestionService questionService;
+
+    public ExaminerServiceImpl(QuestionService questionService) {
+        this.questionService = questionService;
+    }
+
+    @Override
+    public Collection<Question> getQuestions(int amount) {
+        Set<Question> questionSet = new HashSet<>();
+        if (amount > questionService.getAll().size()) {
+            throw new OutOfSizeException();
+        }
+        while (amount < questionSet.size()) {
+            questionSet.add(questionService.getRandomQuestion());
+        }
+        return questionSet;
+    }
+}
